161e953e3559c9546de997b7b198ec8eb2d6d7a2
jontravaille <jk307@duke.edu>
2019-06-28 12:27:22 +0900

    solve House Robber (June 28)

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Categorize by Difficulty
 ### Easy
 ```aidl
+198. House Robber (June 28)
 206. Reverse Linked List (June 27)
 ```
 
@@ -13,10 +14,13 @@
 ## Categorize by Topic (LeetCode Link)
 ### Linked List
 > 206. [Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/)
-
+### Dynamic Programming
+> 192. [House Robber](https://leetcode.com/problems/house-robber/)
 ## Solved
 ```aidl
 206. Reverse Linked List (June 27)
+198. House Robber (June 28)
+
 ```
 ## Not Solved
 
--- src/June/Solution.java
+++ src/June/Solution.java
@@ -1,7 +1,7 @@
 package June;
 
 public class Solution {
-	public ListNode reverseList(ListNode head) { // 206. Reverse Linked List
+	public ListNode reverseList(ListNode head) { // 206. Reverse Linked List (June 27)
 		/*
 		1 -> 2 -> 3 -> 4 -> 5 -> NULL
 
@@ -24,5 +24,39 @@ public class Solution {
 		}
 
 		return curr;
+	}
+
+	public int rob(int[] nums) { // 198. House Robber (June 28)
+		/*
+		Find a maximum amount of money that a thief can rob without alerting the police.
+		A house will contact the police if two adjacent houses were broken into on the same night.
+
+		[1, 2, 3, 1]
+
+		[2, 1, 1, 2]
+		 */
+
+		// edge case
+		if (nums.length < 1) return 0;
+
+		// define variable
+		int max = Integer.MIN_VALUE;
+		int prevRobbed = 0;
+		int prevNotRobbed = 0;
+
+		for (int num: nums) {
+			int currRob = prevNotRobbed + num;
+			int currNotRob = Math.max(prevRobbed, prevNotRobbed);
+			// solved in second try - line 49 is important in a sense that max currNotRob has two choices:
+			// whether currNotRob should have a value of prevRobbed or prevNotRobbed since
+			// Both choices have a possibility of not robbing currently
+
+			max = Math.max(currRob, currNotRob);
+
+			prevRobbed = currRob;
+			prevNotRobbed = currNotRob;
+		}
+
+		return max;
 	}
 }
